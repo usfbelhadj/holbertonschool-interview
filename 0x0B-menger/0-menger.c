17357b4e14fa1c9e266ecdb97f359af91dbb3e68
usfbelhadj <usf.belhadj@gmail.com>
2021-03-04 23:30:24 +0100

    Test

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -6,25 +6,25 @@
  */
 void menger(int level)
 {
-    int n = pow(3, level);
-    int row, col, x, y, c;
+int n = pow(3, level);
+int row, col, x, y, c;
 
-    for (row = 0; row < n; row++)
-    {
-        for (col = 0; col < n; col++)
-        {
-            x = row;
-            y = col;
-            c = '#';
-            while (x || y)
-            {
-                if (x % 3 == 1 && y % 3 == 1)
-                    c = ' ';
-                x /= 3;
-                y /= 3;
-            }
-            putchar(c);
-        }
-        putchar('\n');
-    }
+for (row = 0; row < n; row++)
+{
+for (col = 0; col < n; col++)
+{
+x = row;
+y = col;
+c = '#';
+while (x || y)
+{
+if (x % 3 == 1 && y % 3 == 1)
+c = ' ';
+x /= 3;
+y /= 3;
+}
+putchar(c);
+}
+putchar('\n');
+}
 }
