a3a28dcd14197697dab96899a30bc3c97e814bda
usfbelhadj <usf.belhadj@gmail.com>
2021-06-02 23:39:01 +0100

    Test

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// Star Wars Characters
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];;
+
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const cId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(cId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
