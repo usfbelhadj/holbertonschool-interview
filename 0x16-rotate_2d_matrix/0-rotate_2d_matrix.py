58c9ba0efc72422f75fcdbe6b6bbc61f2eb12381
usfbelhadj <usf.belhadj@gmail.com>
2021-06-10 01:48:12 +0100

    Test

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Rotate 2D Matrix
+'''
+
+
+def rotate_2d_matrix(matrix):
+    N = len(matrix[0])
+    for i in range(N // 2):
+        for j in range(i, N - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[N - 1 - j][i]
+            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
+            matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
+            matrix[j][N - 1 - i] = temp
+    return matrix
