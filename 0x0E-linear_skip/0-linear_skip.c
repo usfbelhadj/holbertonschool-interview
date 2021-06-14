d07383e488582980513bf678e4cd2e9c596a35de
usfbelhadj <usf.belhadj@gmail.com>
2021-06-14 19:14:18 +0100

    Linear skip

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -12,17 +12,17 @@
  */
 skiplist_t *check_in_list(skiplist_t *s, skiplist_t *e, int value)
 {
-	printf("Value found between indexes [%li] and [%li]\n",
-			s->index, e->index);
-	for (; s != e->next;)
-	{
-		printf("Value checked at index [%li] = [%i]\n",
-				s->index, s->n);
-		if (s->n == value)
-			return (s);
-		s = s->next;
-	}
-	return (NULL);
+printf("Value found between indexes [%li] and [%li]\n",
+s->index, e->index);
+for (; s != e->next;)
+{
+printf("Value checked at index [%li] = [%i]\n",
+s->index, s->n);
+if (s->n == value)
+return (s);
+s = s->next;
+}
+return (NULL);
 }
 /**
  * linear_skip - Function that searches for a value
@@ -34,25 +34,25 @@ skiplist_t *check_in_list(skiplist_t *s, skiplist_t *e, int value)
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *s, *e;
+skiplist_t *s, *e;
 
-	if (list == NULL)
-		return (NULL);
-	e = list;
-	while (e != NULL)
-	{
-		s = e;
-		if (e->express)
-			e = e->express;
-		else
-		{
-			for (; e->next;)
-				e = e->next;
-			break;
-		}
-		printf("Value checked at index [%li] = [%i]\n", e->index, e->n);
-		if (e->n >= value)
-			return (check_in_list(s, e, value));
-	}
-	return (check_in_list(s, e, value));
+if (list == NULL)
+return (NULL);
+e = list;
+while (e != NULL)
+{
+s = e;
+if (e->express)
+e = e->express;
+else
+{
+for (; e->next;)
+e = e->next;
+break;
+}
+printf("Value checked at index [%li] = [%i]\n", e->index, e->n);
+if (e->n >= value)
+return (check_in_list(s, e, value));
+}
+return (check_in_list(s, e, value));
 }
