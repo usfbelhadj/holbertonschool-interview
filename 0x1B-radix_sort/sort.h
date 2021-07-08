808e9bf1e55bdce2826b746f2082bce21fd7efcc
usfbelhadj <usf.belhadj@gmail.com>
2021-07-08 23:06:06 +0100

    Push

--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -8,5 +8,5 @@ void print_array(const int *array, size_t size);
 void radix_sort(int *array, size_t size);
 int get_max(int *array, int size);
 int get_digit(int number, int n);
-
+int getLength(int number);
 #endif /* SORT_H */
