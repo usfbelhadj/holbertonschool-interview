de214ede35fc3cc9cffeddde19c8bc04700f2e7d
usfbelhadj <usf.belhadj@gmail.com>
2021-07-08 23:03:33 +0100

    Test

--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -1,8 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
+
 #include <stdlib.h>
+#include <stdio.h>
 void print_array(const int *array, size_t size);
 void radix_sort(int *array, size_t size);
 int get_max(int *array, int size);
