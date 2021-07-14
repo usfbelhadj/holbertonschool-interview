9fc0164cb19a628372cd2f2afbffb270e64e5c09
usfbelhadj <usf.belhadj@gmail.com>
2021-07-14 02:31:21 +0100

    Test

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -9,10 +9,10 @@ def island_perimeter(grid):
     island_perimeter(grid):
     '''
     s = 0
-    count = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                s += 3
-                count += 1
-    return (s + 2) - count
+                s += 4
+                if j > 0 and grid[i][j - 1] == 1: s -= 2
+                if i > 0 and grid[i - 1][j] == 1: s -= 2
+    return s
