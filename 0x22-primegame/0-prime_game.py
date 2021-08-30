2c04bf2dbb53e251c83f72e270e7fd27a23f2a65
usfbelhadj <usf.belhadj@gmail.com>
2021-08-30 16:48:50 +0100

    Test

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -11,10 +11,12 @@ def isWinner(x, nums):
     not_prime = 0
     if nums and x > 0:
         for i in nums:
-            if(i % 2 == 0):
-                prime += 1
-            else:
-                not_prime += 1
+            if (i > 0):
+                if(i % 2 == 0):
+                    prime += 1
+                else:
+                    not_prime += 1
+        print(prime, not_prime)
         if prime >= not_prime:
             return "Maria"
         else:
--- 0x22-primegame/main_0.py
+++ 0x22-primegame/main_0.py
@@ -4,3 +4,4 @@ isWinner = __import__('0-prime_game').isWinner
 
 
 print("Winner: {}".format(isWinner(4, [2, 5, 1, 4, 3])))
+print("Winner: {}".format(isWinner(6, [1, 1, 0, 0, 1, 8])))
