42fd1202c39b2c9f0af462d0bcec71e100fd73b6
usfbelhadj <usf.belhadj@gmail.com>
2021-08-30 16:33:43 +0100

    Test

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -9,7 +9,7 @@ def isWinner(x, nums):
     '''
     prime = 0
     not_prime = 0
-    if nums and x:
+    if nums or x:
         for i in nums:
             if(i % 2 == 0):
                 prime += 1
